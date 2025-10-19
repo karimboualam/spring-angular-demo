2746a84cda113c72dfa7ae4b36596db9e2310640
Karim BOUALAM <boualam.karim94@gmail.com>
2025-10-19 14:48:42 +0200

    fix: use environment.apiUrl for CityService

--- frontend/src/app/services/city.ts
+++ frontend/src/app/services/city.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({ providedIn: 'root' })
 export class CityService {
-  private apiUrl = '/api/cities'; // proxy vers backend (http://localhost:8081/api/cities)
+ // private apiUrl = '/api/cities'; // proxy vers backend (http://localhost:8081/api/cities)
+  private apiUrl = `${environment.apiUrl}/cities`; // utilisation de l'URL depuis environment.ts ou environment.prod.ts
 
   constructor(private http: HttpClient) {}
 
