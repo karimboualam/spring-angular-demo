b9ac9993559dca77da876308197eedef52764474
Karim BOUALAM <boualam.karim94@gmail.com>
2025-10-19 15:08:27 +0200

    update api2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://spring-angular-demo.onrender.com/api/cities' // Corrected URL to point to /api
+  apiUrl: 'https://spring-angular-demo.onrender.com/api' // Corrected URL to point to /api
 };
